28e51f908eb8a785bfe9bd9840070cdc8591d48a
charlene1 <charlene.tormes@gmail.com>
2024-08-31 13:12:35 +0800

    Config github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: "/candyspace/",
+    },
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
-
     css: ["@/assets/css/main.css", "@/assets/css/global.css", "vuetify/styles"],
-
     build: {
         transpile: ["vuetify"],
     },
-
     plugins: ["@/plugins/vuetify", "@/plugins/mirage.ts"],
     modules: ["@pinia/nuxt", "@vueuse/motion/nuxt"],
 });
