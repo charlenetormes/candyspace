38eafadbd6cfc16db2748af77c3fd71cf384b746
charlene1 <charlene.tormes@gmail.com>
2024-08-31 13:19:09 +0800

    Updates

--- _nuxt/SrUHHM9_.js
+++
@@ -1,1 +0,0 @@
-import{f as e,l as o,N as t,o as s,c as a,a as n}from"./Ch3CHRDU.js";const c={class:"p-4"},_=n("h1",null,"Test",-1),r=[_],u=e({__name:"index",setup(l){return o(()=>{t("/calculator")}),(d,i)=>(s(),a("div",c,r))}});export{u as default};
